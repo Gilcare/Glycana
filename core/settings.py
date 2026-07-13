0ff6e25700549f8ce93e7895f417bb0acb2be1c1
Gilcare <gilcare.com@gmail.com>
2026-07-14 00:25:39 +0200

    settings.py
    Application behaviour

---
+++ core/settings.py
@@ -0,0 +1,21 @@
+"""
+Application configuration.
+"""
+
+APP_NAME = "TravenHealth"
+
+APP_VERSION = "0.1.0"
+
+PAGE_TITLE = "TravenHealth"
+
+PAGE_ICON = "🌊"
+
+LAYOUT = "wide"
+
+SIDEBAR_STATE = "expanded"
+
+MAX_CONTENT_WIDTH = 1400
+
+ENABLE_ANIMATIONS = True
+
+ENABLE_DEMO_MODE = True
