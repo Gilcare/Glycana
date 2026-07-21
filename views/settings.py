c1609d2b394228d259c425190027dc4142476cf0
Gilcare <gilcare.com@gmail.com>
2026-07-21 14:20:54 +0200

    settings.py

--- views/settings.py
+++ views/settings.py
@@ -1,1 +1,9 @@
 
+
+def settings_render():
+    render_page_header(
+        title="Settings",
+        subtitle="Explore your glucose patterns."
+    )
+
+    st.info("Settings page under development.")
