65b81b06bc013a3dc2e3bbc1c8691559b9f57360
Gilcare <gilcare.com@gmail.com>
2026-07-21 19:19:03 +0200

    Update glucose.py

--- views/glucose.py
+++ views/glucose.py
@@ -1,9 +1,23 @@
 from core.constants import Pages
 
 def render():
-    render_page_header(
+    render_navbar(
+        page= Pages.GLUCOSE,
+    )
+
+    st.info("Glucose page under development.")
+
+
+
+
+
+
+"""
+def render():
+    render_navbar(
         title="Glucose",
         subtitle="Explore your glucose patterns."
     )
 
     st.info("Glucose page under development.")
+    """
