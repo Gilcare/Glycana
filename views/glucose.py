5e16bcf1db7d3e1da87d6c5dbf0ff5d7a9d77d02
Gilcare <gilcare.com@gmail.com>
2026-07-21 14:13:22 +0200

    glucose.py

--- views/glucose.py
+++ views/glucose.py
@@ -1,1 +1,9 @@
 
+
+def glucose_render():
+    render_page_header(
+        title="Glucose",
+        subtitle="Explore your glucose patterns."
+    )
+
+    st.info("Glucose page under development.")
