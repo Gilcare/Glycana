097b513dabca28d4cbee5f4e04e11ec0ad5ffc86
Gilcare <gilcare.com@gmail.com>
2026-07-23 15:38:43 +0200

    glucose.py

--- views/glucose.py
+++ views/glucose.py
@@ -5,7 +5,37 @@ Glucose View.
 import streamlit as st
 from components import render_page_header
 
+from components.stat_grid import render_stat_grid
 
+
+render_stat_grid(
+
+    stats={
+
+        "Average": "112 mg/dL",
+
+        "Maximum": "168 mg/dL",
+
+        "Minimum": "82 mg/dL",
+
+        "Time in Range": "84%",
+
+        "GMI": "6.1%",
+
+        "CV": "27%",
+
+        "Standard Deviation": "31 mg/dL",
+
+        "Sensor Age": "9 days",
+
+    },
+
+    columns=2,
+
+)
+
+
+"""
 def render():
 
     render_page_header(
@@ -17,3 +47,5 @@ def render():
         icon="🩸",
 
     )
+
+"""
